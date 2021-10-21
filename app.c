50dfa81bb60d49c12e45c272fae0693ff915e5e1
Yoni Kosiner <yoni@kosiner.co.uk>
2021-10-21 13:10:44 +0100

    It works I use c btw now I guess a bit

--- app.c
+++ app.c
@@ -6,12 +6,12 @@ int main() {
     printf("Random Number\n");
     printf("Enter in from 0-5 and take a guess\n");
 
-    bool guess = &guessNumber;
+    bool guess = guessNumber();
 
     if (guess == true) {
-        printf("you were right\n ");
+        printf("you were right\n");
     } else if (guess == false) {
-        printf("you were wrong :c. The number was\n ");
+        printf("you were wrong :c\n");
     };
 
     return 0;
